cdeee04a8be26ef1e93d3384c8cbff4574a3a5a8
OlegPC <jf2.oleg@gmail.com>
2020-03-08 17:56:56 +0200

    Добавил verbose_name для поля и названия таблицы

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50,
+                            verbose_name='Название населенного пункта')
     slug = models.CharField(max_length=50, blank=True)
+    
+    class Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
+        
+    def __str__(self):
+        return self.name
