867c374f94dece803c16d81eed4b7dec6f72ead7
OlegPC <jf2.oleg@gmail.com>
2020-03-15 15:00:58 +0200

    вьюха для отображения записей из таблицы Vacancy

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Vacancy
 
-# Create your views here.
+
+def home_view(request):
+    qs = Vacancy.objects.all()
+    return render(request, 'home.html', {'object_list': qs})
--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from scraping.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('home/', home_view),
 ]
