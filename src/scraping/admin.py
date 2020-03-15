b1a2c8bf2d43ad3bf95e38ef23a62f7e346708cb
OlegPC <jf2.oleg@gmail.com>
2020-03-15 12:32:50 +0200

    Добавил модель Vacancy

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import City, Language
+from .models import City, Language, Vacancy
 
 admin.site.register(City)
 admin.site.register(Language)
+admin.site.register(Vacancy)
--- src/scraping/models.py
+++ src/scraping/models.py
@@ -8,14 +8,14 @@ class City(models.Model):
                             verbose_name='Название населенного пункта',
                             unique=True)
     slug = models.CharField(max_length=50, blank=True, unique=True)
-    
+
     class Meta:
         verbose_name = 'Название населенного пункта'
         verbose_name_plural = 'Название населенных пунктов'
-        
+
     def __str__(self):
         return self.name
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = from_cyrillic_to_eng(str(self.name))
@@ -34,8 +34,27 @@ class Language(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = from_cyrillic_to_eng(str(self.name))
         super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE,
+                             verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='Язык программирования')
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return self.title
