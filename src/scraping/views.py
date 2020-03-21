0b7baf430e6951fe6236985924eb0e4da65c529c
OlegPC <jf2.oleg@gmail.com>
2020-03-21 14:48:56 +0200

    form

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -3,5 +3,6 @@ from .models import Vacancy
 
 
 def home_view(request):
+    print(request.POST)
     qs = Vacancy.objects.all()
     return render(request, 'scraping/home.html', {'object_list': qs})
