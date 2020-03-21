b3308a91f11c29db21fc5eb3f0b759ad30aa36ba
OlegPC <jf2.oleg@gmail.com>
2020-03-21 17:43:11 +0200

    Окончательное оформление формы

--- src/scraping/forms.py
+++ src/scraping/forms.py
@@ -4,5 +4,13 @@ from scraping.models import City, Language
 
 
 class FindForm(forms.Form):
-    city = forms.ModelChoiceField(queryset=City.objects.all())
-    language = forms.ModelChoiceField(queryset=Language.objects.all())
+    city = forms.ModelChoiceField(
+        queryset=City.objects.all(), to_field_name="slug", required=False,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Город'
+    )
+    language = forms.ModelChoiceField(
+        queryset=Language.objects.all(), to_field_name="slug", required=False,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Специальность'
+    )
