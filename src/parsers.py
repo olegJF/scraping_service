409b75ee85170cf9e228809aaee485222caa4361
OlegPC <jf2.oleg@gmail.com>
2020-03-28 14:58:13 +0200

    Добавил функцию для сайта rabota.ua

--- src/parsers.py
+++ src/parsers.py
@@ -34,3 +34,39 @@ def work(url):
         errors.append({'url': url, 'title': "Page do not response"})
 
     return jobs, errors
+
+
+def rabota(url):
+    jobs = []
+    errors = []
+    domain = 'https://rabota.ua'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = BS(resp.content, 'html.parser')
+        main_div = soup.find('div', id='pjax-job-list')
+        if main_div:
+            div_lst = main_div.find_all('div', attrs={'class': 'job-link'})
+            for div in div_lst:
+                title = div.find('h2')
+                href = title.a['href']
+                content = div.p.text
+                company = 'No name'
+                logo = div.find('img')
+                if logo:
+                    company = logo['alt']
+                jobs.append({'title': title.text, 'url': domain + href,
+                             'description': content, 'company': company})
+        else:
+            errors.append({'url': url, 'title': "Div does not exists"})
+    else:
+        errors.append({'url': url, 'title': "Page do not response"})
+
+    return jobs, errors
+
+
+if __name__ == '__main__':
+    url = 'https://rabota.ua/zapros/python/%d0%ba%d0%b8%d0%b5%d0%b2'
+    jobs, errors = rabota(url)
+    h = codecs.open('work.txt', 'w', 'utf-8')
+    h.write(str(jobs))
+    h.close()
