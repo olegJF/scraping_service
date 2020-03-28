90331007ec02b0a637a5583f513c069acbeced3d
OlegPC <jf2.oleg@gmail.com>
2020-03-28 15:28:56 +0200

    Добавил парсер для сайта rabota.ua

--- src/parsers.py
+++ src/parsers.py
@@ -43,21 +43,23 @@ def rabota(url):
     resp = requests.get(url, headers=headers)
     if resp.status_code == 200:
         soup = BS(resp.content, 'html.parser')
-        main_div = soup.find('div', id='pjax-job-list')
-        if main_div:
-            div_lst = main_div.find_all('div', attrs={'class': 'job-link'})
-            for div in div_lst:
-                title = div.find('h2')
-                href = title.a['href']
-                content = div.p.text
-                company = 'No name'
-                logo = div.find('img')
-                if logo:
-                    company = logo['alt']
-                jobs.append({'title': title.text, 'url': domain + href,
-                             'description': content, 'company': company})
+        table = soup.find('table', id='ctl00_content_vacancyList_gridList')
+        if table:
+            tr_lst = table.find_all('tr', attrs={'id': True})
+            for tr in tr_lst:
+                div = tr.find('div',  attrs={'class': 'card-body'})
+                if div:
+                    title = div.find('p',  attrs={'class': 'card-title'})
+                    href = title.a['href']
+                    content = div.p.text
+                    company = 'No name'
+                    p = div.find('p', attrs={'class': 'company-name'})
+                    if p:
+                        company = p.a.text
+                    jobs.append({'title': title.text, 'url': domain + href,
+                                 'description': content, 'company': company})
         else:
-            errors.append({'url': url, 'title': "Div does not exists"})
+            errors.append({'url': url, 'title': "Table does not exists"})
     else:
         errors.append({'url': url, 'title': "Page do not response"})
 
