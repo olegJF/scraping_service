ef8a287464dab936f46682bfa3b3960f9fe209ba
OlegPC <jf2.oleg@gmail.com>
2020-03-28 14:56:49 +0200

    Парсер сайта work.ua

---
+++ src/work.py
@@ -0,0 +1,36 @@
+import requests
+import codecs
+from bs4 import BeautifulSoup as BS
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
+           'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    }
+
+
+def work(url):
+    jobs = []
+    errors = []
+    domain = 'https://www.work.ua'
+    url = 'https://www.work.ua/ru/jobs-kyiv-python/'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = BS(resp.content, 'html.parser')
+        main_div = soup.find('div', id='pjax-job-list')
+        if main_div:
+            div_lst = main_div.find_all('div', attrs={'class': 'job-link'})
+            for div in div_lst:
+                title = div.find('h2')
+                href = title.a['href']
+                content = div.p.text
+                company = 'No name'
+                logo = div.find('img')
+                if logo:
+                    company = logo['alt']
+                jobs.append({'title': title.text, 'url': domain + href,
+                             'description': content, 'company': company})
+        else:
+            errors.append({'url': url, 'title': "Div does not exists"})
+    else:
+        errors.append({'url': url, 'title': "Page do not response"})
+
+    return jobs, errors
