7f5ccc40764888dd6edd55d9603e3d939b3d9048
OlegPC <jf2.oleg@gmail.com>
2020-03-29 13:15:27 +0300

    Добавление вакансий в БД, полученных в результате скрапинга сайтов

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,5 +1,6 @@
 import codecs
 import os, sys
+from django.db import DatabaseError
 
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
@@ -18,14 +19,22 @@ parsers = (
     (djinni, 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python'),
     (rabota, 'https://rabota.ua/zapros/python/%d0%ba%d0%b8%d0%b5%d0%b2')
 )
-city = City.objects.filter(slug='kiev')
+city = City.objects.filter(slug='kiev').first()
+language = Language.objects.filter(slug='python').first()
+
 jobs, errors = [], []
 for func, url in parsers:
     j, e = func(url)
     jobs += j
     errors += e
 
+for job in jobs:
+    v = Vacancy(**job, city=city, language=language)
+    try:
+        v.save()
+    except DatabaseError:
+        pass
 
-h = codecs.open('work.txt', 'w', 'utf-8')
-h.write(str(jobs))
-h.close()
+# h = codecs.open('work.txt', 'w', 'utf-8')
+# h.write(str(jobs))
+# h.close()
