dca56ff597925b9be86372b25cd53bb4dc4fd7de
OlegPC <jf2.oleg@gmail.com>
2020-03-29 12:36:10 +0300

    django.setup()

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,6 +1,16 @@
 import codecs
+import os, sys
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
+
+import django
+django.setup()
 
 from scraping.parsers import *
+
+from scraping.models import Vacancy, City, Language
 
 parsers = (
     (work, 'https://www.work.ua/ru/jobs-kyiv-python/'),
@@ -8,12 +18,13 @@ parsers = (
     (djinni, 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python'),
     (rabota, 'https://rabota.ua/zapros/python/%d0%ba%d0%b8%d0%b5%d0%b2')
 )
-
+city = City.objects.filter(slug='kiev')
 jobs, errors = [], []
 for func, url in parsers:
     j, e = func(url)
     jobs += j
     errors += e
+
 
 h = codecs.open('work.txt', 'w', 'utf-8')
 h.write(str(jobs))
