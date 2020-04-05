7090d1e4a3ad34d497800d2dd99b3e2398ff7580
OlegPC <jf2.oleg@gmail.com>
2020-04-05 13:10:17 +0300

    Добавил модель URL

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import City, Language, Vacancy, Error
+from .models import City, Language, Vacancy, Error, Url
 
 admin.site.register(City)
 admin.site.register(Language)
 admin.site.register(Vacancy)
 admin.site.register(Error)
+admin.site.register(Url)
--- src/scraping/models.py
+++ src/scraping/models.py
@@ -4,6 +4,10 @@ from django.db import models
 from scraping.utils import from_cyrillic_to_eng
 
 
+def default_urls():
+    return {"work": "", "rabota": "", "dou": "", "djinni": ""}
+
+
 class City(models.Model):
     name = models.CharField(max_length=50,
                             verbose_name='Название населенного пункта',
@@ -65,3 +69,13 @@ class Error(models.Model):
     timestamp = models.DateField(auto_now_add=True)
     data = jsonfield.JSONField()
 
+
+class Url(models.Model):
+    city = models.ForeignKey('City', on_delete=models.CASCADE,
+                             verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='Язык программирования')
+    url_data = jsonfield.JSONField(default=default_urls)
+    
+    class Meta:
+        unique_together = ("city", "language")
