fb1d2e9867866032fa4abe6352236fd4b0561830
OlegPC <jf2.oleg@gmail.com>
2020-04-11 11:16:26 +0300

    Получение наборов урлов для функций парсинга согласно предпочтений пользователей

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -12,7 +12,7 @@ import django
 django.setup()
 
 from scraping.parsers import *
-from scraping.models import Vacancy, City, Language, Error
+from scraping.models import Vacancy, City, Language, Error, Url
 
 User = get_user_model()
 
@@ -29,7 +29,21 @@ def get_settings():
     settings_lst = set((q['city_id'], q['language_id']) for q in qs)
     return settings_lst
 
+
+def get_urls(_settings):
+    qs = Url.objects.all().values()
+    url_dct = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+    urls = []
+    for pair in _settings:
+        tmp = {}
+        tmp['city'] = pair[0]
+        tmp['language'] = pair[1]
+        tmp['url_data'] = url_dct[pair]
+        urls.append(tmp)
+    return urls
+
 q = get_settings()
+u = get_urls(q)
 
 city = City.objects.filter(slug='kiev').first()
 language = Language.objects.filter(slug='python').first()
