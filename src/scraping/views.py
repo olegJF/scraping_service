02467c31c0de3ffbabb96d2c6d234518d79b00d3
OlegPC <jf2.oleg@gmail.com>
2020-04-11 17:17:18 +0300

    Добавил пагинацию

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .forms import FindForm
@@ -7,9 +8,16 @@ from .models import Vacancy
 def home_view(request):
     # print(request.GET)
     form = FindForm()
+
+    return render(request, 'scraping/home.html', {'form': form})
+
+
+def list_view(request):
+    # print(request.GET)
+    form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
-    qs = []
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = {}
         if city:
@@ -18,5 +26,9 @@ def home_view(request):
             _filter['language__slug'] = language
 
         qs = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping/home.html', {'object_list': qs,
-                                                  'form': form})
+        paginator = Paginator(qs, 10)  # Show 10 contacts per page.
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['object_list'] = page_obj
+    return render(request, 'scraping/list.html', context)
--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from scraping.views import home_view
+from scraping.views import home_view, list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_view),
+    path('list/', list_view, name='list'),
+    path('', home_view, name='home'),
 ]
