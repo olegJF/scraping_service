b428c13190f800ab492b375e28ad1345eb2797d4
OlegPC <jf2.oleg@gmail.com>
2020-04-18 16:46:40 +0300

    Отправки писем пользователям. Функция готова.

--- src/send_emails.py
+++ src/send_emails.py
@@ -9,12 +9,41 @@ sys.path.append(proj)
 os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 
 django.setup()
+from scraping.models import Vacancy
+from scraping_service.settings import EMAIL_HOST_USER
+subject = "Рассылка вакансий"
+text_content = "Рассылка вакансий"
+from_email = EMAIL_HOST_USER
+empty = '<h2>К сожалению на сегодня по Вашим предпочтениям данных нет. </h2>'
+User = get_user_model()
+qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
+users_dct = {}
+for i in qs:
+    users_dct.setdefault((i['city'], i['language']), [])
+    users_dct[(i['city'], i['language'])].append(i['email'])
+if users_dct:
+    params = {'city_id__in': [], 'language_id__in': []}
+    for pair in users_dct.keys():
+        params['city_id__in'].append(pair[0])
+        params['language_id__in'].append(pair[1])
+    qs = Vacancy.objects.filter(**params).values()[:10]
+    vacancies = {}
+    for i in qs:
+        vacancies.setdefault((i['city_id'], i['language_id']), [])
+        vacancies[(i['city_id'], i['language_id'])].append(i)
+    for keys, emails in users_dct.items():
+        rows = vacancies.get(keys, [])
+        html = ''
+        for row in rows:
+            html += f'<h5"><a href="{ row["url"] }">{ row["title"] }</a></h5>'
+            html += f'<p>{row["description"]} </p>'
+            html += f'<p>{row["company"]} </p><br><hr>'
+        _html = html if html else empty
+        for email in emails:
+            to = email
+            msg = EmailMultiAlternatives(subject, text_content, from_email,
+                                         [to])
+            msg.attach_alternative(_html, "text/html")
+            msg.send()
 
 
-
-subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
-text_content = 'This is an important message.'
-html_content = '<p>This is an <strong>important</strong> message.</p>'
-msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-msg.attach_alternative(html_content, "text/html")
-msg.send()
