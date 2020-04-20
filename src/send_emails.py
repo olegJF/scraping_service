a6aac76a537feeaf0c3c16e0ad58ce1cff09715b
OlegPC <jf2.oleg@gmail.com>
2020-04-20 14:22:51 +0300

    Формирование данных письма админу с описанием пары город\ЯП, для которых отсутствуют урлы в таблице Url.

--- src/send_emails.py
+++ src/send_emails.py
@@ -10,8 +10,12 @@ sys.path.append(proj)
 os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 
 django.setup()
-from scraping.models import Vacancy, Error
-from scraping_service.settings import EMAIL_HOST_USER
+from scraping.models import Vacancy, Error, Url
+from scraping_service.settings import (
+    EMAIL_HOST_USER,
+    EMAIL_HOST, EMAIL_HOST_PASSWORD
+)
+
 ADMIN_USER = EMAIL_HOST_USER
 
 today = datetime.date.today()
@@ -33,36 +37,68 @@ if users_dct:
         params['language_id__in'].append(pair[1])
     qs = Vacancy.objects.filter(**params, timestamp=today).values()
     vacancies = {}
-    # for i in qs:
-    #     vacancies.setdefault((i['city_id'], i['language_id']), [])
-    #     vacancies[(i['city_id'], i['language_id'])].append(i)
-    # for keys, emails in users_dct.items():
-    #     rows = vacancies.get(keys, [])
-    #     html = ''
-    #     for row in rows:
-    #         html += f'<h3"><a href="{ row["url"] }">{ row["title"] }</a></h3>'
-    #         html += f'<p>{row["description"]} </p>'
-    #         html += f'<p>{row["company"]} </p><br><hr>'
-    #     _html = html if html else empty
-    #     for email in emails:
-    #         to = email
-    #         msg = EmailMultiAlternatives(
-    #             subject, text_content, from_email, [to]
-    #         )
-    #         msg.attach_alternative(_html, "text/html")
-    #         msg.send()
+    for i in qs:
+        vacancies.setdefault((i['city_id'], i['language_id']), [])
+        vacancies[(i['city_id'], i['language_id'])].append(i)
+    for keys, emails in users_dct.items():
+        rows = vacancies.get(keys, [])
+        html = ''
+        for row in rows:
+            html += f'<h3"><a href="{ row["url"] }">{ row["title"] }</a></h3>'
+            html += f'<p>{row["description"]} </p>'
+            html += f'<p>{row["company"]} </p><br><hr>'
+        _html = html if html else empty
+        for email in emails:
+            to = email
+            msg = EmailMultiAlternatives(
+                subject, text_content, from_email, [to]
+            )
+            msg.attach_alternative(_html, "text/html")
+            msg.send()
 
 qs = Error.objects.filter(timestamp=today)
+subject = ''
+text_content = ''
+to = ADMIN_USER
+_html = ''
 if qs.exists():
     error = qs.first()
     data = error.data
-    _html = ''
     for i in data:
         _html += f'<p"><a href="{ i["url"] }">Error: { i["title"] }</a></p><br>'
-    subject = f"Ошибки скрапинга {today}"
-    text_content = "Ошибки скрапинга"
-    to = ADMIN_USER
+    subject += f"Ошибки скрапинга {today}"
+    text_content += "Ошибки скрапинга"
+
+qs = Url.objects.all().values('city', 'language')
+urls_dct = {(i['city'], i['language']): True for i in qs}
+urls_err = ''
+for keys in users_dct.keys():
+    if keys not in urls_dct:
+        urls_err += f'<p"> Для города: {keys[0]} и ЯП: {keys[1]} отсутствуют урлы</p><br>'
+if urls_err:
+    subject += ' Отсутствующие урлы'
+    _html += urls_err
+
+if subject:
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(_html, "text/html")
     msg.send()
 
+# import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+#
+# msg = MIMEMultipart('alternative')
+# msg['Subject'] = 'Список вакансий за  {}'.format(today)
+# msg['From'] = EMAIL_HOST_USER
+# mail = smtplib.SMTP()
+# mail.connect(EMAIL_HOST, 25)
+# mail.ehlo()
+# mail.starttls()
+# mail.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+#
+# html_m = "<h1>Hello world</h1>"
+# part = MIMEText(html_m, 'html')
+# msg.attach(part)
+# mail.sendmail(EMAIL_HOST_USER, [to], msg.as_string())
+# mail.quit()
