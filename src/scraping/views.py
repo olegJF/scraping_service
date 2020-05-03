2693158b3f2a1dc3536c08406df136b190b6384e
OlegPC <jf2.oleg@gmail.com>
2020-05-03 12:28:35 +0300

    Вывод списка записей на основе ListView

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from .forms import FindForm
 from .models import Vacancy
@@ -45,3 +45,33 @@ class VDetail(DetailView):
     queryset = Vacancy.objects.all()
     template_name = 'scraping/detail.html'
     # context_object_name = 'object'
+
+
+class VList(ListView):
+    model = Vacancy
+    template_name = 'scraping/list.html'
+    form = FindForm()
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['city'] = self.request.GET.get('city')
+        context['language'] = self.request.GET.get('language')
+        context['form'] = self.form
+
+        return context
+
+    def get_queryset(self):
+        city = self.request.GET.get('city')
+        language = self.request.GET.get('language')
+        qs = []
+        if city or language:
+            _filter = {}
+            if city:
+                _filter['city__slug'] = city
+            if language:
+                _filter['language__slug'] = language
+            qs = Vacancy.objects.filter(**_filter)
+        return qs
+
--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from scraping.views import home_view, list_view, v_detail, VDetail
+from scraping.views import home_view, list_view, v_detail, VDetail, VList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('list/', list_view, name='list'),
+    # path('list/', VList.as_view(), name='list'),
     path('accounts/', include(('accounts.urls', 'accounts'))),
     path('detail/<int:pk>/', VDetail.as_view(), name='detail'),
     path('', home_view, name='home'),
